0be6edf67224cc4c8b44e01de164181c3bac00e1
寒兮 <IceSoulHanxi@outlook.com>
2021-03-20 23:53:57 +0800

    chore: 初始化Gradle模块化SpringBoot项目
    添加common模块与web模块

---
+++ blog-web/src/test/java/com/ixnah/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ixnah.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
